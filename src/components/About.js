a02bd209295683fd9061e1a80a2039b346e9ef41
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-01-11 17:25:45 +0300

    returning react function in JSX syntax

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p>I love creating beautiful user interfaces and giving customer a wonderful experience.</p>
+    <img src={image}></img>
+
+  </div>
+  );
 }
 
 export default About;
